02aa23513b56b5010d803cebf02466ab93d17a50
Henric Nilsson <henric.nilsson01@gmail.com>
2026-09-23 21:00:24 +0200

    fixed version
    fixed version

--- Balance.cs
+++ Balance.cs
@@ -7,65 +7,59 @@ class Balance
     public static void Main()
     {
         bool isBalanced = true;
-        while (true)
+
+        string s = Console.ReadLine();
+
+        Stack<char> stack = new Stack<char>();
+
+        for (int i = 0; i < s.Length; i++)
         {
-
-            Console.Write("Enter '( )' or '[ ]' combination: ");
-            string s = Console.ReadLine();
-
-            Stack<char> stack = new Stack<char>();
-
-            for (int i = 0; i < s.Length; i++)
+            // Standardfall om det inte är en giltig input
+            if (s[i] != '(' && s[i] != ')' && s[i] != '[' && s[i] != ']')
             {
+                isBalanced = false;
+                break;
+            }
 
-                //Default case if it is not a valid input, we will break the loop and return 0
-                if (s[i] != '(' && s[i] != ')' && s[i] != '[' && s[i] != ']')
+            // Öppna paranteser eller brackets pusha på stacken.
+            if (s[i] == '(' || s[i] == '[')
+            {
+                stack.Push(s[i]);
+                continue;
+            }
+
+            // Stängande parentes
+            if (s[i] == ')')
+            {
+                if (stack.Count == 0 || stack.Peek() != '(')
                 {
-                    Console.WriteLine("Invalid input. Please enter only '(', ')', '[' or ']'.");
                     isBalanced = false;
                     break;
                 }
 
-                // Check if the _input variable is "(" 0r "["
-                //because we dont need to check if it is "]" or ")", 
-                if (s[i] == '(' || s[i] == '[')
-                {
-                    stack.Push(s[i]);
-                    continue;
-                }
-
-                // Only need to check with the further iterations if it is a right sided parantheses.
-                if (s[i] == ')')
-                {
-                    if (stack.Count == 0 || stack.Peek() != '(')
-                    {
-                        isBalanced = false;
-                        break;
-                    }
-                    //pop the stack to make it a clean slate for the next iteration
-                    stack.Pop();
-                }
-
-                // Only need to check with the further iterations if it is a right sided square bracket. 
-                if (s[i] == ']')
-                {
-                    if (stack.Count == 0 || stack.Peek() != '[')
-                    {
-                        isBalanced = false;
-                        break;
-                    }
-                    //pop the stack to make it a clean slate for the next iteration
-                    stack.Pop();
-                }
-
-
+                stack.Pop();
             }
-            if (stack.Count > 0)
+
+            // Stängande bracket 
+            if (s[i] == ']')
             {
-                isBalanced = false;
-            }
+                if (stack.Count == 0 || stack.Peek() != '[')
+                {
+                    isBalanced = false;
+                    break;
+                }
 
-            Console.WriteLine(isBalanced ? "1" : "0");
+                stack.Pop();
+            }
         }
+
+        // Om något finns kvar på stacken,
+        // finns det obalanserade öppningsparenteser eller brackets.
+        if (stack.Count > 0)
+        {
+            isBalanced = false;
+        }
+
+        Console.WriteLine(isBalanced ? "1" : "0");
     }
 }
